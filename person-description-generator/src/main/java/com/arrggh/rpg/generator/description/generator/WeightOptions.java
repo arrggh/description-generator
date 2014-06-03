181c0d957286db94e81b2fb8118014cae14a41c3
arrggh <coder@arrggh.com>
2014-06-03 18:13:47 +0100

    Fixed issue where the Height and Weight combo boxes were missing the random option.

--- person-description-generator/src/main/java/com/arrggh/rpg/generator/description/generator/WeightOptions.java
+++ person-description-generator/src/main/java/com/arrggh/rpg/generator/description/generator/WeightOptions.java
@@ -12,7 +12,7 @@ public enum WeightOptions {
     Heavy("Heavy", 1, 2),
     VeryHeavy("Very Heavy", 2, 3),
     ExtremelyHeavy("Extremely Heavy", 3, 4);
-    
+
     private static Set<WeightOptions> NonRandomOptions = new HashSet<>();
     static {
         NonRandomOptions.add(ExtremelyLight);
@@ -23,45 +23,46 @@ public enum WeightOptions {
         NonRandomOptions.add(VeryHeavy);
         NonRandomOptions.add(ExtremelyHeavy);
     }
-    
-    private static String[] WeightTexts = new String[] {ExtremelyLight.getText(),
+
+    private static String[] WeightTexts = new String[] {Random.getText(),
+                                                        ExtremelyLight.getText(),
                                                         VeryLight.getText(),
                                                         Light.getText(),
                                                         Average.getText(),
                                                         Heavy.getText(),
                                                         VeryHeavy.getText(),
                                                         ExtremelyHeavy.getText()};
-    
+
     private String text;
     private int minStdDev;
     private int maxStdDev;
-    
+
     private WeightOptions(String text, int minStdDev, int maxStdDev) {
         this.text = text;
         this.minStdDev = minStdDev;
         this.maxStdDev = maxStdDev;
     }
-    
+
     public String getText() {
         return text;
     }
-    
+
     public int getMinStdDev() {
         return minStdDev;
     }
-    
+
     public int getMaxStdDev() {
         return maxStdDev;
     }
-    
+
     public static String[] getWeights() {
         return WeightTexts;
     }
-    
+
     public static Set<WeightOptions> getNonRandomValues() {
         return NonRandomOptions;
     }
-    
+
     public static WeightOptions getWeight(String selectedWeight) {
         for (final WeightOptions weight : values()) {
             if (weight.getText().equals(selectedWeight)) {
